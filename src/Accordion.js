7bda7fa004d2faa373757f3a612c2660760e0a53
Feifan Zeng <fzze@student.unimelb.edu.au>
2020-11-27 14:49:58 +1030

    create Accordion.js

---
+++ src/Accordion.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export class Accordion extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isOpen : false };
+        this.handleExpand = this.handleExpand.bind(this);
+    }
+
+    handleExpand() {
+        const newState = this.state.isOpen === true ? false : true;
+        this.setState({ isOpen: newState });
+    }
+
+    render() {
+        return(
+            <div className="box">
+                <div className="title-box" onClick={this.handleExpand}>
+                    <span className="title">{this.props.title}</span>
+                    <span className="icon"><i className={`fa fa-play-circle${!this.state.isOpen ? ' down' : ''}`}></i></span>
+                    <div className="clearfix"></div>
+                </div>
+                {this.state.isOpen && <div className="content">{this.props.children}</div>}
+            </div>
+        )
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Accordion} from './Accordion.js'
 
 import React, { useState } from "react"
 
@@ -14,25 +15,11 @@ function App() {
       <Accordion title="Korea">
         Sample text
       </Accordion>
-      <Accordion title="England" isExpand="true">
+      <Accordion title="England" isOpen="true">
         Sample text
       </Accordion>
     </div>
   );
-}
-
-const Accordion = ({ children, title, isExpand = false }) => {
-  const [expand, setExpand] = useState(isExpand);
-  return (
-    <div className="box">
-      <div className="title-box" onClick={() => setExpand(expand => !expand)}>
-        <span className="title">{title}</span>
-        <span className="icon"><i className={`fa fa-play-circle${!expand ? ' down' : ''}`}></i></span>
-        <div className="clearfix"></div>
-      </div>
-      {expand && <div className="content">{children}</div>}
-    </div>
-  )
 }
 
 export default App;
